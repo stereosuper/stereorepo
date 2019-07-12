dcf221afcfda218576e4f54fd7da5d740f1b2a12
Alphability <albanmezino@gmail.com>
2019-07-12 19:48:00 +0200

    🎉 init
    Signed-off-by: Alphability <albanmezino@gmail.com>

---
+++ packages/sac/src/index.js
@@ -0,0 +1,23 @@
+import {
+    roundNumbers,
+    forEach,
+    reverseString,
+    createNewEvent,
+    requestAnimFrame,
+    throttle,
+    query,
+    isDisplayed,
+    bodyRouter,
+} from './core';
+
+export default {
+    roundNumbers,
+    forEach,
+    reverseString,
+    createNewEvent,
+    requestAnimFrame,
+    throttle,
+    query,
+    isDisplayed,
+    bodyRouter,
+};
