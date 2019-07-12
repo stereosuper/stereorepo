b6102224965d93d82833210373ec424a74e90763
Alphability <albanmezino@gmail.com>
2019-07-12 23:57:30 +0200

    🎨 sprite package init vanilla
    Signed-off-by: Alphability <albanmezino@gmail.com>

---
+++ packages/sprite/src/index.js
@@ -0,0 +1,10 @@
+import SpriteComponent from './components/Sprite';
+import SpriteVueComponent from './components/Sprite.vue';
+
+export const Sprite = SpriteComponent;
+export const SpriteVue = SpriteVueComponent;
+
+export default {
+    Sprite,
+    SpriteVue,
+};
