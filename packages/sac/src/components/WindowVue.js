db7ee26cea700ab3e6175498acc73a601b6d7e63
Alphability <albanmezino@gmail.com>
2019-10-10 11:00:39 +0200

    ✨ sac: superWindowVue errors and store

--- packages/sac/src/components/WindowVue.js
+++ packages/sac/src/components/WindowVue.js
@@ -1,24 +1,33 @@
-export const toggleNoScroll = ({ noScroll, nextTick }) => {
-    if (noScroll) {
-        document.documentElement.style.top = `${-window.scrollY}px`;
-        document.documentElement.classList.add('no-scroll');
-    } else {
-        const scrollY = Math.abs(
-            parseInt(document.documentElement.style.top.replace('px', ''), 10)
-        );
-        document.documentElement.style.top = '';
-        document.documentElement.classList.remove('no-scroll');
+import WindowError from './Error';
 
-        nextTick(() => {
-            window.scrollTo(0, scrollY);
-        }, 0);
+const checkStoreValue = ({ store, methodeName }) => {
+    if (!store) {
+        throw new WindowError(
+            `No store found, try passing vuex store as an argument in ${methodeName} call`
+        );
     }
 };
 
 const setWindowSize = store => {
-    if (!store.state.stereoWindow) {
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    store.commit('superWindow/setWindow', {
+        width: windowWidth,
+        height: windowHeight
+    });
+};
+
+const launchWindow = store => {
+    requestAnimationFrame(() => {
+        setWindowSize(store);
+    });
+};
+
+export const initializeWindow = store => {
+    checkStoreValue({ store, methodeName: 'initializeWindow' });
+    if (!store.state.superWindow) {
         store.registerModule(
-            'stereoWindow',
+            'superWindow',
             {
                 namespaced: true,
                 state: {
@@ -36,32 +45,38 @@ const setWindowSize = store => {
         );
     }
 
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    store.commit('stereoWindow/setWindow', {
-        width: windowWidth,
-        height: windowHeight
-    });
-};
-
-const launchWindow = store => {
-    requestAnimationFrame(() => {
-        setWindowSize(store);
-    });
-};
-
-export const initializeWindow = store => {
     setWindowSize(store);
 
     window.addEventListener('resize', launchWindow.bind(null, store), false);
 };
 
-export const destroyWindow = () => {
+export const destroyWindow = store => {
     window.removeEventListener('resize', launchWindow);
+    checkStoreValue({ store, methodeName: 'destroyWindow' });
+    if (store.state.superWindow) {
+        store.unregisterModule('superWindow');
+    }
+};
+
+export const toggleNoScroll = ({ noScroll, nextTick }) => {
+    if (noScroll) {
+        document.documentElement.style.top = `${-window.scrollY}px`;
+        document.documentElement.classList.add('no-scroll');
+    } else {
+        const scrollY = Math.abs(
+            parseInt(document.documentElement.style.top.replace('px', ''), 10)
+        );
+        document.documentElement.style.top = '';
+        document.documentElement.classList.remove('no-scroll');
+
+        nextTick(() => {
+            window.scrollTo(0, scrollY);
+        }, 0);
+    }
 };
 
 const install = Vue => {
-    Vue.prototype.$stereorepo.window = {
+    Vue.prototype.$stereorepo.superWindow = {
         toggleNoScroll,
         initializeWindow,
         destroyWindow
