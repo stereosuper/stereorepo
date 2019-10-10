2255224bd5fd6e1802d2797f6926692747938eb1
Alphability <albanmezino@gmail.com>
2019-10-10 13:12:52 +0200

    🔧 build function + eslint update

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                include: ['@babel/plugin-transform-spread'],
+            },
+        ],
+    ],
+    plugins: [
+        '@babel/plugin-syntax-dynamic-import',
+        '@babel/plugin-transform-spread',
+    ],
+};
